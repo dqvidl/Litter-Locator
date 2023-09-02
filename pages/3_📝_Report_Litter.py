112dc1afb139eb5fd7e730312e65caacde936bb2
david liang <96744843+dqvidl@users.noreply.github.com>
2023-09-02 06:00:22 -0400

    Update 3_📝_Report_Litter.py

--- pages/3_📝_Report_Litter.py
+++ pages/3_📝_Report_Litter.py
@@ -11,9 +11,9 @@ st.header("Report Litter")
 st.write("Report litter polluted areas")
 
 address = st.text_input("Address")
-
+data = []
 df = pd.DataFrame(
     np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
     columns=['lat', 'lon'])
 
-st.map(df)
+st.map(data, zoom = 7.5)
