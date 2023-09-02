96ad6219a5569cfb60c1399373265231473c7398
david liang <96744843+dqvidl@users.noreply.github.com>
2023-09-02 04:51:39 -0400

    geocode rerun

--- pages/2_🔍_Find_Litter.py
+++ pages/2_🔍_Find_Litter.py
@@ -1,17 +1,19 @@
 import streamlit as st
-# import pandas as pd
+import pandas as pd
+
 st.set_page_config(
   page_title = "Litter Locator",
   page_icon = "🌲",
   layout = "wide"
 ) 
+
 st.header("Find Litter")
 st.write("Search for litter polluted areas near you")
 
-# locator = Nominatim(user_agent= "myGeocoder")
-# address = st.text_input("Address")
-# search_radius = st.slider("Search Radius (kilometers)", min_value=1, max_value=100, step=1, value=10)
+locator = Nominatim(user_agent= "myGeocoder")
+address = st.text_input("Address")
+search_radius = st.slider("Search Radius (kilometers)", min_value=1, max_value=100, step=1, value=10)
 
-# if st.button("Search"):
-#   location = geolocator.geocode(address)
-#   st.write(f'{location.latitude}, {location, longitude}')
+if st.button("Search"):
+  location = geolocator.geocode(address)
+  st.write(f'{location.latitude}, {location, longitude}')
