20bf897ef3a5dd0c06a006d3e91100adbda005dc
david liang <96744843+dqvidl@users.noreply.github.com>
2023-09-02 08:50:34 -0400

    geocode alternate approach

--- pages/2_🔍_Find_Litter.py
+++ pages/2_🔍_Find_Litter.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
-# from geopy.geocoders import Nominatim
+import requests
+import urllib.parse
+
 
 st.set_page_config(
   page_title = "Litter Locator",
@@ -11,10 +13,12 @@ st.set_page_config(
 st.header("Find Litter")
 st.write("Search for litter polluted areas near you")
 
-# locator = Nominatim(user_agent= "myGeocoder")
-address = st.text_input("Address")
+address = 'Shivaji Nagar, Bangalore, KA 560001'
+# address = st.text_input("Address")
+url = 'https://nominatim.openstreetmap.org/search/' + urllib.parse.quote(address) +'?format=json'
 search_radius = st.slider("Search Radius (kilometers)", min_value=1, max_value=100, step=1, value=10)
 
-# if st.button("Search"):
-#   location = geolocator.geocode(address)
-#   st.write(f'{location.latitude}, {location.longitude}')
+if st.button("Search"):
+  response = requests.get(url).json()
+  location = geolocator.geocode(address)
+  st.write(f'{response[0]["lat"]}, {response[0]["lon"]}')
