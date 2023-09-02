159d1c3d216d104a30c16e796c666fba20e570ec
Daw <133544883+DawDa07@users.noreply.github.com>
2023-09-02 08:51:59 -0400

    Update 3_📝_Report_Litter.py

--- pages/3_📝_Report_Litter.py
+++ pages/3_📝_Report_Litter.py
@@ -19,21 +19,21 @@ st.write('You selected:', option)
 
 if option == "Address":
   address = st.text_input("Address")
-elif option == 'Drop pin on map':
+elif option == "Drop pin on map":
   df = pd.DataFrame(
     np.random.randn(100, 2) / [50, 50] + [43.46, -80.52],
     columns=['lat', 'lon'])
 
   st.map(df)
 
-st.subheader('Picture')
+st.subheader("Picture")
 
-pictureorno = st.selectbox('Would you like to provide photographic evidence of the region?', 'YES', 'NO')
+pictureorno = st.selectbox("Would you like to provide photographic evidence of the region?", 'YES', 'NO')
 
 if pictureorno == 'YES':
   picture = st.camera_input("Take a picture")
   if picture:
     st.image(picture)
 
-st.subtext('Thank you')
+st.subtext("Thank you")
 
