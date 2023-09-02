f80a0afea89de01de747406102c8a26f5747216e
david liang <96744843+dqvidl@users.noreply.github.com>
2023-09-02 05:20:09 -0400

    Update 3_Report_Litter.py

--- pages/3_Report_Litter.py
+++ pages/3_Report_Litter.py
@@ -1,2 +1,11 @@
-st.title("Report Litter")
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(
+  page_title = "Litter Locator",
+  page_icon = "🌲",
+  layout = "wide"
+) 
+
+st.header("Report Litter")
 st.write("THIS IS A TEST")
