d21d5acf81ee05fb6febf7924f0b34885de5195a
david liang <96744843+dqvidl@users.noreply.github.com>
2023-09-02 05:54:52 -0400

    Update 3_📝_Report_Litter.py

--- pages/3_📝_Report_Litter.py
+++ pages/3_📝_Report_Litter.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-
+import numpy as np
 st.set_page_config(
   page_title = "Litter Locator",
   page_icon = "🌲",
@@ -12,3 +12,8 @@ st.write("Report litter polluted areas")
 
 address = st.text_input("Address")
 
+df = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(df)
