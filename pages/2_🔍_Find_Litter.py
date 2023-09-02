d86a7501c7a999e5f5a77aea84f64bc670c9c74a
david liang <96744843+dqvidl@users.noreply.github.com>
2023-09-02 04:36:28 -0400

    debugging

--- pages/2_🔍_Find_Litter.py
+++ pages/2_🔍_Find_Litter.py
@@ -4,6 +4,6 @@ st.title("Find Litter")
 st.write("Search for litter polluted areas near you")
 st.map(reported_locations)
 
-locator = Nominatim(user_agent=”myGeocoder”)
+locator = Nominatim(user_agent= "myGeocoder")
 location = geolocator.geocode("175 5th Avenue NYC")
 search_radius = st.slider("Search Radius (kilometers)", min_value=1, max_value=100, step=1, value=10)
