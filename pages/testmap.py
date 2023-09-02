86ae48f5515469004c2c520a0642de3ee7f5ca9b
Daw <133544883+DawDa07@users.noreply.github.com>
2023-09-02 15:07:46 -0400

    Update testmap.py

--- pages/testmap.py
+++ pages/testmap.py
@@ -1,1 +1,20 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from geopy.geocoders import Nominatim
 
+
+st.set_page_config(
+  page_title = "Litter Locator",
+  page_icon = "🌲",
+  layout = "wide"
+) 
+
+data = []
+
+data.append({
+  "Latitude" : 43.46,
+  "Longitude" : -80.52
+})
+
+st.map(data)
