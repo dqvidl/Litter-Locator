2812ce4f2fa4408f7170240b51a37bddea4cf30f
david liang <96744843+dqvidl@users.noreply.github.com>
2023-09-02 07:00:36 -0400

    Update 3_📝_Report_Litter.py

--- pages/3_📝_Report_Litter.py
+++ pages/3_📝_Report_Litter.py
@@ -12,7 +12,7 @@ st.write("Report litter polluted areas")
 
 option = st.selectbox(
     'How would you like to report a littered area?',
-    ('Drop pin on map', 'Address'))
+    ('Drop pin on map', 'Address', 'picture'))
 
 st.write('You selected:', option)
 
