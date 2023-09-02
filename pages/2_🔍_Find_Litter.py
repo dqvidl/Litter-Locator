02d04d29efa8bc2ea96266eddd37a52605117b66
david liang <96744843+dqvidl@users.noreply.github.com>
2023-09-02 04:34:15 -0400

    Geocode testing

--- pages/2_🔍_Find_Litter.py
+++ pages/2_🔍_Find_Litter.py
@@ -1,1 +1,9 @@
+reported_locations = pd.DataFrame(columns=["Location Name", "Description", "Latitude", "Longitude"])
 
+st.title("Find Litter")
+st.write("Search for litter polluted areas near you")
+st.map(reported_locations)
+
+locator = Nominatim(user_agent=”myGeocoder”)
+location = geolocator.geocode("175 5th Avenue NYC")
+search_radius = st.slider("Search Radius (kilometers)", min_value=1, max_value=100, step=1, value=10)
