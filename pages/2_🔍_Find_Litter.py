0916340200536c8e6965094353d345b91c9d09a2
david liang <96744843+dqvidl@users.noreply.github.com>
2023-09-02 04:46:39 -0400

    Update 2_🔍_Find_Litter.py

--- pages/2_🔍_Find_Litter.py
+++ pages/2_🔍_Find_Litter.py
@@ -1,13 +1,13 @@
-import streamlit as st
-import pandas as pd
+# import streamlit as st
+# import pandas as pd
 
 st.title("Find Litter")
 st.write("Search for litter polluted areas near you")
 
-locator = Nominatim(user_agent= "myGeocoder")
-address = st.text_input("Address")
-search_radius = st.slider("Search Radius (kilometers)", min_value=1, max_value=100, step=1, value=10)
+# locator = Nominatim(user_agent= "myGeocoder")
+# address = st.text_input("Address")
+# search_radius = st.slider("Search Radius (kilometers)", min_value=1, max_value=100, step=1, value=10)
 
-if st.button("Search"):
-  location = geolocator.geocode(address)
-  st.write(f'{location.latitude}, {location, longitude}')
+# if st.button("Search"):
+#   location = geolocator.geocode(address)
+#   st.write(f'{location.latitude}, {location, longitude}')
