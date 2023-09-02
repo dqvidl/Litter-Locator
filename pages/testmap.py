2a832fb31f5308673e79a22ee337bef0366724cf
Daw <133544883+DawDa07@users.noreply.github.com>
2023-09-02 15:18:43 -0400

    Update testmap.py

--- pages/testmap.py
+++ pages/testmap.py
@@ -2,18 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+df = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
 
-st.set_page_config(
-  page_title = "Litter Locator",
-  page_icon = "🌲",
-  layout = "wide"
-) 
-
-data = []
-
-data.append({
-  "Latitude" : 43.46,
-  "Longitude" : -80.52
-})
-
-st.map({43.46}, {-80.52})
+st.map(df)
