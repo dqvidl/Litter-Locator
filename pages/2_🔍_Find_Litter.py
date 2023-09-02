9149f1ae5f17c9e568d71c22f8b9081e57b47834
david liang <96744843+dqvidl@users.noreply.github.com>
2023-09-02 04:44:11 -0400

    address to coordinate geocode test

--- pages/2_🔍_Find_Litter.py
+++ pages/2_🔍_Find_Litter.py
@@ -1,9 +1,13 @@
-reported_locations = pd.DataFrame(columns=["Location Name", "Description", "Latitude", "Longitude"])
+# reported_locations = pd.DataFrame(columns=["Location Name", "Description", "Latitude", "Longitude"])
 
 st.title("Find Litter")
 st.write("Search for litter polluted areas near you")
-st.map(reported_locations)
+# st.map(reported_locations)
 
 locator = Nominatim(user_agent= "myGeocoder")
-location = geolocator.geocode("175 5th Avenue NYC")
+address = st.text_input("Address")
 search_radius = st.slider("Search Radius (kilometers)", min_value=1, max_value=100, step=1, value=10)
+
+if st.button("Search"):
+  location = geolocator.geocode(address)
+  st.write(f'{location.latitude}, {location, longitude}')
