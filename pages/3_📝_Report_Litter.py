d0f3da5e34da93730b8101d1f2ab6f9b771def1d
david liang <96744843+dqvidl@users.noreply.github.com>
2023-09-02 06:56:43 -0400

    Update 3_📝_Report_Litter.py

--- pages/3_📝_Report_Litter.py
+++ pages/3_📝_Report_Litter.py
@@ -10,11 +10,20 @@ st.set_page_config(
 st.header("Report Litter")
 st.write("Report litter polluted areas")
 
-address = st.text_input("Address")
+"""
 data = []
 ACCELERATOR_CENTER = (43.477351051348485, -80.54952878983036)
 df = pd.DataFrame(
     np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
     columns=['lat', 'lon'])
 
-st.map(ACCELERATOR_CENTER, zoom = 7.5)
+st.map(ACCELERATOR_CENTER, zoom = 7.5)"""
+
+option = st.selectbox(
+    'How would you like to report a littered area?',
+    ('Address', 'Drop pin on map'))
+
+st.write('You selected:', option)
+
+if option = address:
+  address = st.text_input("Address")
