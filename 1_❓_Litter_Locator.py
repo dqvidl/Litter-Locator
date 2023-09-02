0d0e458d523f081e3fc0b4875f4a3d6e4bcd85d3
david liang <96744843+dqvidl@users.noreply.github.com>
2023-09-02 03:11:00 -0400

    Updated information in landing page

--- 1_❓_Litter_Locator.py
+++ 1_❓_Litter_Locator.py
@@ -5,5 +5,10 @@ st.set_page_config(
   layout = "wide"
 ) 
 
-st.title("Litter Locator")
+st.title("About Litter Locator")
+st.write('Did you know that in the USA, there are more than 50 billion pieces of litter along roads and waterways? This is why we present Litter Locator. Litter Locator is an innovative solution to the problem of urban littering. The main aim of Litter Locator is to bring together a community of people who share the common goal of promoting cleaner and healthier surroundings. The website has two key features - users can report and locate litter. ")
+
+st.header("Our goal")
+
+
 st.sidebar.success('test')
