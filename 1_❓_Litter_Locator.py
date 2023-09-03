1c9b012698a743ac7fd135317c52ab0aa7851b6d
Daw <133544883+DawDa07@users.noreply.github.com>
2023-09-02 21:02:32 -0400

    Update 1_❓_Litter_Locator.py

--- 1_❓_Litter_Locator.py
+++ 1_❓_Litter_Locator.py
@@ -11,7 +11,7 @@ st.set_page_config(
 
 st.title("About Litter Locator")
 image = Image.open('environment.jpg')
-st.image(image, caption = 'Help Jamal reduce littering')
+st.image(image, caption = 'Help reduce littering')
 st.write("Did you know that in the United States alone, an astonishing 51 billion pieces of litter are left on the ground every year? This shocking statistic highlights a pervasive issue that not only harms the environment but also degrades the way our communities are presented. After all, no one likes seeing a litter ridden city. In response to this alarming problem, Litter Locator has emerged as a beacon of hope in the battle against litter.  Litter Locator is not just a website; it's a powerful tool for change. By harnessing the collective efforts of concerned citizens, it presents a novel approach to reducing litter. Anyone can use the platform to self-report large quantities of litter in their communities, pinpointing trouble spots that need urgent attention. What sets Litter Locator apart is its ingenious system of mobilizing teams of dedicated cleaners who access the reported locations on the website, swiftly locating and cleaning these litter-ridden areas.  This dynamic synergy between technology and community engagement not only helps to keep our surroundings clean but also fosters a sense of shared responsibility. Litter Locator isn't just cleaning up our streets; it's cleaning up our collective conscience. Join the movement and be a part of the solution. Together, we can make a world with less litter and more beauty.")
 
 st.divider()
